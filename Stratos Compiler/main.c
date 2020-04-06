50e023ab6529e009f55f85a8f4e565bc4853cf39
arthur-kamau <kamaukenn11@gmail.com>
2020-04-06 20:33:13 +0300

    reading from a file

--- Stratos Compiler/main.c
+++ Stratos Compiler/main.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main()
 {
-    printf("Hello world!\n");
+   //read file
+   readFile();
+
     return 0;
+}
+
+void readFile(){
+
+
+     char path[] = {"/home/publisher/Development/stratos/src/main.st"};
+
+     char data[6000];
+     FILE *fptr ;
+
+
+     fptr = fopen(path, "r");
+
+     if(fptr == NULL){
+         printf("errror");
+         exit(1);
+     }else{
+        printf("file exist \n");
+     }
+
+   char currentline[100];
+
+  while (fgets(currentline, sizeof(currentline), fptr) != NULL) {
+        fprintf(stderr, "%s\n", currentline);
+        /* Do something with `currentline` */
+    }
+
+     fclose(fptr);
 }
